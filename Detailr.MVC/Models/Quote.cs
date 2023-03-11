ea2c72e1cc08b35bad294ea1c94ce34bc3de0c6f
olive <olive@OLLIES>
2023-03-11 15:31:04 +0000

    Functionality for quote search results and started the design for the main page

---
+++ Detailr.MVC/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿namespace Detailr.MVC.Models
+{
+    public class Quote
+    {
+        public string Postcode { get; set; }
+        public int Radius { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        
+    }
+}
